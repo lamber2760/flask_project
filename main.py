168b9a2cdf4d4d0bc6f6d8d90fded62c2b7734ac
Name <sachin981648276@gmail.com>
2024-04-04 13:46:13 +0530

    flask2nd

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from flask import Flask
-from flask import render_template,request
+from flask import render_template,request,redirect
+
+
+
+
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 
 app=Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///myfirstdb.db"
+db=SQLAlchemy(app)
+
+class contactUs(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    Title = db.Column(db.String(120))
+    Message = db.Column(db.Text)
+
+with app.app_context():
+    db.create_all() 
+
+
+
 
 @app.route("/")
 def homepage():
@@ -18,16 +38,21 @@ def aboutus():
 def contactus():
     return render_template("contactus.html")
 
-@app.route("/sevices")
+@app.route("/services")
 def services():
     return render_template("services.html")
 
 @app.route("/savethisdata",methods =["post"])
 def savethisdata():
-    if request.method =="POST":
-        mytitile=request.form.get("title")
+    if request.method  =="POST":
+        mytitle=request.form.get("title")
         message=request.form.get("msg")
-        print(mytitile,message)
+ 
+        data = contactUs(Title=mytitle,Message=message)
+        db.session.add(data)
+        db.session.commit()
+        return redirect("/contactus")  
+        
     return"this is datt savee................"
  
 if __name__ =="__main__":
